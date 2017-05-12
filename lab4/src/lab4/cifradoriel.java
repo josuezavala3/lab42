5c02086552dd05bb37c81a6c0151082da54bd536
josue <wilmerjosues@yahoo.com>
2017-05-12 16:35:50 -0600

    puller

---
+++ lab4/src/lab4/cifradoriel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+/**
+ *
+ * @author HP
+ */
+public class cifradoriel {
+
+    private int[] matriz(String clave) {
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        return null;
+    }
+}
